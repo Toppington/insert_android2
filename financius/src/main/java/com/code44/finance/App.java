1ad7df99fe87c80ebf535497185324b87a4a4d3b
Spencer Harwood <spencerharwood@MacBook-Pro.local>
2017-09-18 16:58:30 -0400

    Spencer's Insert Implementation

--- financius/src/main/java/com/code44/finance/App.java
+++ financius/src/main/java/com/code44/finance/App.java
@@ -9,6 +9,8 @@ import dagger.ObjectGraph;
 import hugo.weaving.DebugLog;
 import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
 
+import sdk.insert.io.*;
+
 public class App extends Application {
     private ObjectGraph objectGraph;
 
@@ -17,6 +19,13 @@ public class App extends Application {
     }
 
     @Override public void onCreate() {
+
+        Insert.initSDK(
+                this,
+                "92da87638f3232464241110cabbb01b7cebe4c0ac13b2733808726f285637b7e8bfd14be1857edf7a19cefdbbf4f53bd5616c9f1d4f6aef52caf5591c70245582f516b95947e796c62238641367a564c.cd13fa1fc8c372706cc8dd50d5380a4a.2d9baff8d20cadceb651584b86d4c8d5accf64cd25211367cf0db185cd37e176",
+                "sthtest",
+                null);
+
         super.onCreate();
 //        StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder()
 //                .detectAll()
--- financius/src/main/java/com/code44/finance/api/requests/RegisterDeviceRequest.java
+++ financius/src/main/java/com/code44/finance/api/requests/RegisterDeviceRequest.java
@@ -11,41 +11,41 @@ import com.code44.finance.common.utils.Preconditions;
 import com.google.android.gms.gcm.GoogleCloudMessaging;
 
 public class RegisterDeviceRequest extends Request {
-    private static final String PROJECT_NUMBER = "1007413878843";
-
-    private final Context context;
-    private final Users usersService;
-    private final GcmRegistration gcmRegistration;
-
+//    private static final String PROJECT_NUMBER = "1007413878843";
+//
+//    private final Context context;
+//    private final Users usersService;
+//    private final GcmRegistration gcmRegistration;
+//
     public RegisterDeviceRequest(Context context, Users usersService, GcmRegistration gcmRegistration) {
         super(null);
-
-        Preconditions.notNull(context, "Context cannot be null.");
-        Preconditions.notNull(usersService, "Users cannot be null.");
-        Preconditions.notNull(gcmRegistration, "GCM Registration cannot be null.");
-
-        this.context = context;
-        this.usersService = usersService;
-        this.gcmRegistration = gcmRegistration;
-    }
-
+//
+//        Preconditions.notNull(context, "Context cannot be null.");
+//        Preconditions.notNull(usersService, "Users cannot be null.");
+//        Preconditions.notNull(gcmRegistration, "GCM Registration cannot be null.");
+//
+//        this.context = context;
+//        this.usersService = usersService;
+//        this.gcmRegistration = gcmRegistration;
+   }
+//
     @Override protected Object performRequest() throws Exception {
-        String registrationId = gcmRegistration.getRegistrationId();
-
-        // Register with GCM if necessary
-        if (TextUtils.isEmpty(registrationId)) {
-            final GoogleCloudMessaging gcm = GoogleCloudMessaging.getInstance(context);
-            registrationId = gcm.register(PROJECT_NUMBER);
-            gcmRegistration.setRegistrationId(registrationId);
-        }
-
-        // Prepare registration body
-        final RegisterDeviceBody body = new RegisterDeviceBody();
-        body.setRegId(registrationId);
-
-        // Register device with AppEngine
-        usersService.registerDevice(body).execute();
-        gcmRegistration.setRegisteredWithServer(true);
+//        String registrationId = gcmRegistration.getRegistrationId();
+//
+//        // Register with GCM if necessary
+//        if (TextUtils.isEmpty(registrationId)) {
+//            final GoogleCloudMessaging gcm = GoogleCloudMessaging.getInstance(context);
+//            registrationId = gcm.register(PROJECT_NUMBER);
+//            gcmRegistration.setRegistrationId(registrationId);
+//        }
+//
+//        // Prepare registration body
+//        final RegisterDeviceBody body = new RegisterDeviceBody();
+//        body.setRegId(registrationId);
+//
+//        // Register device with AppEngine
+//        usersService.registerDevice(body).execute();
+//        gcmRegistration.setRegisteredWithServer(true);
         return null;
     }
 }
